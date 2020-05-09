bc6beaa02dd942545611c6275e802a1a52a58781
Edwin Villa <villaceniceros@gmail.com>
2020-05-09 13:47:30 -0600

    Completed angelas challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MagicBall());
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNumber = 1;
+
+  void changeBallNumber (){
+    ballNumber = Random().nextInt(5) +1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent[700],
+        body: Center(
+          child: FlatButton(
+            onPressed: (){
+              setState((){
+                changeBallNumber();
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+            ),
+        ),
       ),
     );
+  }
+}
